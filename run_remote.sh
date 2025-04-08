513b23827305f0245aca3e74339a3681cb5c946d
dij0s <redrouge03@gmail.com>
2025-04-08 10:40:54 +0200

    Created remote agent script deployment..

---
+++ run_remote.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SSH connection details
+HOST="192.168.237.135"
+USER="raspi"
+
+# Local agent folder path
+LOCAL_AGENT_PATH="./agent"
+# Remote path where to copy the agent folder
+REMOTE_PATH="/home/raspi/app"
+
+# Copy everything into the remote server
+scp -r ./* ${USER}@${HOST}:${REMOTE_PATH}
+
+# Then connect and restart docker compose
+echo "Starting the stack in attached mode..."
+ssh -t ${USER}@${HOST} 'cd /home/raspi/app && docker compose down && docker compose up'
+
+echo "Docker-compose stopped.."
